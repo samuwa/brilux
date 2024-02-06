07ae47bdac85337c900b2bda7c6b5633710461a8
samuwa <69830704+samuwa@users.noreply.github.com>
2024-02-06 10:55:09 -0500

    Create Home.py

---
+++ Home.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.title("Reportes Brilux :roll_of_paper:")
+
+st.markdown("####")
+
+st.write(":arrow_left:    Los reportes están en la barra a la izquierda.")
+
+st.write("Monta el archivo Excel en la barra y revisa los reportes.")
