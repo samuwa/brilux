3d68bc5209e885c7dd4101fdf3b09500be5ffbd4
samuwa <69830704+samuwa@users.noreply.github.com>
2024-04-30 12:59:21 -0500

    Update functions.py

--- functions.py
+++ functions.py
@@ -110,7 +110,8 @@ def filter_prefixes(description):
 
 def preprocess_data(df, inicio, cierre):
     # Convert 'Document Date' to datetime format
-    df = df[df['SOP Type'] == "Pedido"]
+    df['Document Date'] = df.apply(lambda x: x['Order Date'] if x['Order Date'] != pd.Timestamp('1900-01-01') else x['Document Date'], axis=1)
+    df = df[df['SOP Type'] == "Factura"]
     df['Document Date'] = pd.to_datetime(df['Document Date'])
     # df = df[df["Document Date"] >= "20240201"]
     # df = df[df["Document Date"] <= "20240301"]
