f01936cb2774f2ad058540ba52d0424293b48af5
samuwa <69830704+samuwa@users.noreply.github.com>
2024-06-07 09:22:15 -0500

    Update Home.py

--- Home.py
+++ Home.py
@@ -125,7 +125,7 @@ if isinstance(df_sin_cxc, pd.DataFrame) and isinstance(df_cxc, pd.DataFrame):
 
 
 # Seleccionar un reporte a visualizar
-reporte = st.sidebar.selectbox("Selecciona un reporte", ["Diario - Pedidos", "Mensual - Pedidos", "CXC", "Detallado Cadenas", "Ventas SCI", "Análisis Vendedores"])
+reporte = st.sidebar.selectbox("Selecciona un reporte", ["Diario - Pedidos", "Mensual - Pedidos", "CXC", "Detallado Cadenas", "Ventas SCI", "Análisis Vendedores", "Proyección Clientes"])
 
 
 
@@ -850,6 +850,175 @@ elif reporte == "Análisis Vendedores":
 
 
 
+
+elif reporte == "Proyección Clientes":
+
+    
+    clientes = ["DISTRIBUIDORA OLIMAN, C.A.","DECOPLASTICOS UNION, C.A.","EXCELSIOR GAMA SUPERMERCADOS, C.A.","PANDOCK CARACAS, C.A.","TELARES LOS ANDES, S.A.","AUTOMERCADO LA MURALLA, C.A.","AUTOMERCADOS PLAZA'S, C.A.","BODEGON JJ 18,C.A","UNIVERSIDAD METROPOLITANA","DISTRIBUIDORA VIEJO ALMACEN, C.A.","B2B.CCS, C.A.","COMERCIAL OFERTON 668, C.A.","DAFRAN DISTRIBUIDORA, C.A.","FERRETERIA EPA, C.A.","REDVITAL COMERCIALIZADORA,C.A.","DISTRIBUIDORA LUGO LEON ,C.A.","UNION ISRAELITA DE CARACAS","PAPELES Y ALGO MAS 2071, C.A.","CORPORACION MAXIPLAST,C.A.","SUMILARCA, C.A.","BRICKMAN CORP.C.A.","RAFAEL EDUARDO DEL ROSARIO VIVAS","SAMDICA CARACAS, C.A.","CONDOMINIO TORRE DIGITEL","MULTISERVICIOS PROKLEAN, C.A.","CENTROBECO, C.A.","MUNDO PAPEL 2001, C.A.","TUCAREN, C.A.","ARMANDO PINTO","DISTRIBUIDORA DE SERVICIOS JAS,C.A","ABRAHAM WAINBERG","DISTRIBUIDORA TOP TEN 18,C.A.","BANCO PROVINCIAL, S.A. BANCO UNIVERSAL","FERRETOTAL CARACAS, C.A","INVERSIONES SATORNO JC, C.A.","CENTRAL MADEIRENSE, C.A.","DISTRIBUIDORA DANUBIO, C.A.","DISTRIBUIDORA EUROEXPRESS 94, C.A.","FERRETERIA EL MUNDO GEOTEXTIL 7999, C.A.","INVERSIONES GLOBAL PACK, C.A.","AUTOMERCADO LUZ, C.A.","COMERCIAL ABU ALI XXI, C.A.","INVERSIONES KIPLIN, C.A.","GABRIEL ALEXANDER HERNANDEZ DE MENDONCA","COMERCIALIZADORA MONTI SIN LIMITES 1528, C.A.","CONSORCIO BASENTO, C.A.","DISTRIBUIDORA LA SUPERIOR 300, C.A.","GLOBAL EXPRESSER,C.A.","FABRICA DE SEGURIDAD GONZALEZ, C.A.","DISTRIBUIDORA DOMI ZONA, C.A.","SANCHEZ CASTRO SANDRA SIOMARA","LA MALETA DEL UNICENTRO, C.A.","JEAN CARLOS MARTINEZ ROJAS","COMERCIAL FUERZA PLAST 168, C.A.","DISTRIBUIDORA GR 2019, C.A.","DIVEPA DISTRIBUIDORA VENEZOLANA DE PAPEL, C.A.","DIPROLIMA, C.A.","COMERCIAL BUENA PLAST 888, C.A.","SUPERMERCADOS UNICASA, C.A.","AUTOMERCADOS PLAZA'S, C.A(San Bernardino)","INVERSIONES C.G.A., C.A.","CLUB CAMURI GRANDE AC","PLANSUAREZ, C.A.","DISTRIBUIDORA GONCALVES Y MORANTES 2009, C.A.","SUMIPAN,C.A","AUTOMERCADOS PLAZA'S, C.A.(Vista Alegre)","AUTOMERCADOS PLAZA'S, C.A. (El Paraiso)","AUTOMERCADOS PLAZA'S, C.A.(Los Chaguaramos)","COMERCIALIZADORA BARUTITO 3000, C.A.","COMERCIALIZADORA LA GITANA","COMERCIALIZADORA CEGA CLEAN,C.A","A.C. CENTRO MEDICO DOCENTE LA TRINIDAD","AUTOMERCADOS PLAZA'S, C.A.(La Lagunita)","AUTOMERCADOS PLAZA'S, C.A.(Valle Arriba)","JOHNSON DISTRIBUCIONES, C.A.","DISTRIBUIDORA CHANCHAMAYO, C.A.","SUMINISTROS CLEAN 21, C.A.","DUAN SUMINISTROS, C.A.","AUTOMERCADOS PLAZA' S,C.A. (Santa Eduvigis)","AUTOMERCADOS PLAZA'S, C.A.(Prado)","AUTOMERCADOS PLAZA'S, C.A.(Buenaventura)","AUTOMERCADOS PLAZA'S, C.A.(El Cafetal)","AUTOMERCADOS PLAZA'S, C.A.(Hiper Guatire)","AUTOMERCADOS PLAZA'S,C.A(Higuerote)","COMERCIALIZADORA ARANDA R&A, C.A.","AUTOMERCADOS PLAZA'S,C.A.(Los Nisperos)","AUTOMERCADOS PLAZA'S, C.A.(Galerias)","AUTOMERCADOS PLAZA'S,C.A(San Antonio)","AUTOMERCADOS PLAZA'S, C.A.(Veracruz)","AUTOMERCADOS PLAZA'S, C.A.(Centro Plaza)","AUTOMERCADOS PLAZA'S, C.A.(Los Cedros)","SUPERMERCADO WENDYS L.M.D.R., C.A.","CENTRO SOCIAL CULT. Y DEPORT. HEBRAICA, A.C.","ASOCIACION ISRAELITA DE VENEZUELA","SUPERMERCADO RIO VIDA, C.A.","REPRESENTACIONES REDELTA 1708, C.A.","FERRRETERIA EPA, C.A.","PROVERCLEAN 0407, C.A.","IND VZLNA DE SEGURIDAD INDUSTRIAL INVENSI C.A.","JAVIER ALFREDO PAEZ OROZCO","MUEBLES DOÑA BARBARA,C.A","AUTOMERCADOS PLAZA'S, C.A.(Avila)","PLASTICOS MILANO, C.A.","INVERSIONES TORMES, C.A.","PETIT QUIM FERRET, C.A.","COMERCIALIZADORA PICO BOLIVAR, C.A.","A.A. PLASTICOS 18, C.A.","INVERSIONES DISGLOSUM, C.A.","MARIA IRUÑA URRUTICOECHEA RIOS","OKALIA, C.A.","BANCO NACIONAL DE CREDITO,C.A BANCO UNIVERSAL","AUTOMERCADOS PLAZA'S, C.A(El Rosal)","LOS CAMPITOS III AUTOMERCADOS, C.A.","INVERSIONES RANKIT 0721, C.A.","DISTRIBUIDORA SHUNTONG, C.A","PANDOCK DE VALENCIA, C.A.","PANDOCK DE MARACAIBO, C.A.","D.A.G. INTERNACIONAL, C.A.","PANDOCK DEL TACHIRA, C.A.","PANDOCK MERIDA, C.A.","PANDOCK LOS ANDES, C.A.","PANDOCK DE BARQUISIMETO, C.A.","COMERCIAL MINI PLAS 168, C.A.","MIREYA INES MORENO DAVILA","AMAYRA DE LOURDES VELON URBANEJA","MALETERIA EL TURISTA, C.A.","INVERSIONES BALZA PAREDES, C.A.","SAHER GROUP 1509, C.A.","DISTRIBUIDORA CA-R1, C.A.","DISTRIBUIDORA R.P.P, C.A.","MI NEGOCIO SUPERMERCADOS, C.A.","DISTRIBUIDORA JY 2020,C.A.","INVERSIONES G.B 21, C.A.","COMERCIAL VIDA FELIZ, C.A.","SOFIA G SANTANIELLO JAIMES","RICHARD BARAZARTE PEREZ","MIAMI EXPRESS 2018, C.A.","INVERSIONES VIAJERO DEL FUTURO, C.A.","INVERSIONES PANAMERICANA MILENIUM, C.A.","OFERTA PLUS, C.A.","GREIS SAMIRA  ARCILA VERA","INVERSIONES CHARBYST, C.A.","COMERCIAL ELDO, C.A.","ORGANIZACION BRACLEH, C.A.","COOPERATIVA NOVEDADES MARTA 02042 R.L.","COMERCIAL TURVEN, C.A.","AUTOMERCADO SOL CARIOCA III, C.A.","CHA CHA AUTOMERCADO, C.A.","INVERSIONES MADVEN 2015, C.A.","FLC,C.A","LA FERIA DEL PLASTICO,C.A","HIPERCANDY DEL CENTRO, C.A.","UNIPLAST 3M, C.A.","INVERSIONES ELYM 2315, C.A.","COMERCIAL 6688 LIU,C.A.","INVERSIONES JMPINTO 2012,C.A.","FERRETERIA IP-5, C.A.","CARLOS EDUARDO APONTE YANES","GESTION INTEGRAL DE VALORES INMOBILIARIOS,C.A","CAMPIFERRETERIA, C.A.","GRUPO DLG 18, C.A.","INDUVAR, S.A.","DISTRIBUIDORA LOS MOROCHOS 2009,C.A","INVERSIONES ARIZONA GOURMET,C.","DISTRIBUIDORA WILVEN, C.A.","AUTOMERCADOS PLAZA'S, C.A.(Los Samanes)","LICORERIA MENDI EDER,C.A","RENOVADOS DE CAUCHO GUARENAS,C.A.","FULLER MANTENIMIENTO, C.A.","FUGUET ALBA & ASOCIADOS","DISTRIBUIDORA FAMA CENTER,C.A","DISTRIBUIDORA PLASTIEMPAQUES QUINTA CRESPO, C.A.","ALIANZA SOCIEDAD DE CORRETAJE DE VALORES, C.A.","FABRICA BRILUX,C.A","AUTOMERCADO SANTA ROSA DE LIMA, C.A.","ADELA BARRETO RANGEL","COMERCIAL JORMANO, C.A.","AUTOMERCADO LICARCH, C.A.","OFIYANZA, C.A.","AUTOMERCADO SAN LORENZO, C.A.","COMERCIALIZADORA SAVI-LOR, C.A.","INVERSIONES LOS REYES DEL CHALECO, C.A.","RECICLAJE PALO VERDE II, C.A.","RECUPERADORA LLANERA, C.A.","CORPORACION DIGITEL, C.A.","AUTOMERCADOS PLAZA'S, C.A.(Los Naranjos)","RODRIGUEZ MEDINA FREIDUAL JOSE","EXPRESS 212, C.A.","PROLIFE, C.A.","FARMATODO, C.A.","DISTRIBUIDORA AJPS,C.A.","SICOBEN DISTRIBUIDORA, C.A.","AUTOMERCADOS PLAZA'S, C.A. (Carrizal)","INVERSIONES FABRICIO V, C.A.","HIPERMERCADO PETARE, C.A.","AUTOMERCADO SANTA PAULA, C.A.","VIVE SALUD LAB, C.A","ALNOVA, C.A.","SAIN ANTONIO PRADO GERARDINO","INVERSIONES LA GRAN FAMILIA 2021, C.A.","TIENDA LA ESPAÑA, C.A.","CASA JUANES,C.A","DIXON EDUARDO LOPEZ CALDERON","COMERCIALIZADORA CENTRO GUARENAS, C.A.","MARAMBIO,RIVILLO,PEREZ,PINEDA,CONT PUB, S.C.","MARKET FAVORITA 286, C.A.","FRUTERIA LOS POMELOS, C.A.","HIPERMERCADO PARAMO, C.A.","COMERCIAL LUCKY STAR, C.A.","DISTRIBUIDORA PLASTIEMPAQUE, C.A.","AUTOMERCADO SUPER ECONOMICO 23, C.A.","SUPERMERCADO PALO VERDE, C.A.","RESIDENCIAS ALTAMIRANA","SUPERMERCADO LAS MINAS DE BARUTA 2000, C.A.","CORMUNDO JS, C.A.","QUINCALLA HERMANOS CINCO, C.A.","TRAKI DISTRIBUIDORA C.A","INVERSIONES LA QUESERIA DE DOÑA SOCORRO, C.A.","INVERSIONES PRACTYHOGAR, C.A.","JAVIER JIMENEZ ROJAS","INVERSIONES TAPIAS 2023,C.A","KAMAR 2529, C.A.","REINALDO EVARISTO BARRETO GALARRAGA","GRUPO FLORESTAL,C.A","PRODUCTOS DE LIMPIEZA FOX, C.A.","GIUSMARI STELLA TARANTINO LUNA","JEAN CARLOS GOUVEIA GALLARDO","COLCHONES GULF COAST SD, C.A.","A.S. 17 LIDO, C.A.","FERRETOTAL CARACAS, C.A.","AUTOMERCADO LICARCH II, C.A.","IMPORTADORA JJAC 21, C.A.","FRAZZANI SPORT, C.A.","INDUSTRIAS DE CALZADOS OBELISCO, C.A.","RIVILLO DIAZ,MARIA DE LOS ANGELES","ZAPATERIA Y TALABARTERIA HADAYA,C.A","CREACIONES GIANLOP, C.A.","ARTES GRAFICAS REY, C.A.","DISTRIBUIDORA FULL PROGRESO,C.A","COMERCIAL LIDER STAR, C.A.","PLUMROSE LATINOAMERICANA,C.A","INVERSIONES LIDERPAN, C.A.","COMERCIAL MAROMA, C.A.","ENVASES CARACAS, C.A.","DISTRIBUIDORA GROUPG 2014, C.A.","MUEBLERIA AMANDA1805,C.A.","INVERSIONES SAN ELIAS 2022, C.A.","C.A. CENTRO MEDICO DE CARACAS","SUPERMERCADO PIEDRA AZUL, C.A.","TRANSPORTE Y COMERCIALIZADORA NJG, C.A.","IMPORTACIONES LUCKY FENG, C.A.","BRANZA 1800, C.A.","MERCANTIL TODOLANDIA IMPORT, C.A.","MERCANTIL POYO, C.A.","COMERCIAL SOLMIPLAST, C.A.","HOME READY DEL ESTE, C.A.","AUTOMERCADOS PLAZA'S, C.A.(El Parral)","INVERSIONES A.J.M. 1969, C.A.","EUROBUILDING INTERNACIONAL, C.A.","INVERSIONES TOTALMAX 1984, C.A.","BANCO DEL CARIBE,C.A. BANCO UNIVERSAL","DISTRIBUIDORA MADISON DEL LITORAL, C.A.","LA CIUDAD DEL MORRAL, C.A.","DISTRIBUIDORA MADISON MARGARITA, C.A.","INVERSIONES J.M.A.C, C.A","REPRESENTACIONES EL GRAN ESFUERZO 2012, C.A","INVERSIONES MEGAPIXEL, C.A.","JOHN HENRY PEREZ BARON","FABRICA INTERPLAS, C.A.","INVERSIONES BAZAR FERRETERIA JC 2200, C.A.","RUTH NOEMI BLANCO RUIZ","FESTEJOS MAR, C.A.","ALEXANDER JAVIER BRITO SILVA","DISTRIBUIDORA LA NUEVA VIDA 2011, C.A.","COMERCIAL LOS RUICES, C.A.","DISTRIBUIDORA LIMPAVI, C.A.","PANDOCK DEL ORINOCO, C.A.","DISTRIBUIDORA LA JOYA 2000, C.A.","JARDIN PLANTARVEN 805, C.A.","S.S. VENEZUELA, C.A.","INVERSIONES MAGICCLEAN 1802, C.A.","SUPERMERCADO LOS HERMANOS VALERO 2019, C.A.","LAGUNITA COUNTRY CLUB, A.C.","INVERSIONES HOTELERAS 7070, C.A.","A.C.TODOKASHER","SILDEC SUMINISTROS, C.A.","DISTRIBUIDORA SAJAMAR, C.A.","COMERCIAL FORTUNA 27, C.A.","ANTONIO JESUS ORTA MARTINEZ","SERVICIOS Y TRANSPORTES ISETH, C.A","TIENDAS MILSALDOS, C.A.","COMERCIAL PUNTO LEC UNA, C.A.","DISTRIBUIDORA MAXI MAYOR QUINTA CRESPO, C.A.","TELAS GUAYANA, S.R.L.","INVERSIONES FADELVA, C.A.","CARNES Y VIVERES EL DORADO, C.A.","RECUPERADORA DEL LLANO, C.A.","MANGOCENTER,C.A.","NEXOMART, C.A.","ALMACENES LA OFERTA, C.A.","GALERIAS LINFER, C.A.","INVERSIONES PAUMAR, C.A.","CENTRO FERRETERO EL PICO, C.A.","SUMINISTROS OPCATE, C.A.","DISTRIBUIDORA QUINTA MAYOR, C.A.","COMERCIAL WIPLAST, C.A.","TROPICAL SERVICE, C.A.","SUMIOF 1956, C.A.","FERRETOTAL CARACAS, C.A.(03 Panamericana)","TODO EN DESECHABLE, C.A.","CARTON DE VENEZUELA, S.A.","SUPERMERCADO LOS PROCERES, C.A.","ANAKARINA FERMIN R","FERRETERIA EL PICO, C.A"]
+
+    vendedores = ["RZ","OFC","OFC","OFC","VB SCI","MJ SCI","JD SCI","OFC","VB SCI","HGE","OFC","HGE","OFC","OFC","OFC","OFC","OFC","APT","OFC","MDN","MDN","OFC","OFC","OFC","OFC","DAL","OFC","OFC","OFC","APT","OFC","OFC","VB SCI","OFC","APT","OFC","APT","APT","OFC","APT","JM SCI","DAL","DAL","SM","JD SCI","DAL","OFC","DAL","RZ","OFC","OFC","DAL","SM","HGE","HGE","OFC","APT","HGE","OFC","JM SCI","ALAN  ","VB SCI","JM SCI","OFC","MDN","JM SCI","JM SCI","JM SCI","MJ SCI","VM SCI","MDN","VB SCI","JM SCI","JM SCI","APT","HGE","MJ SCI","APT","JM SCI","JM SCI","JM SCI","JM SCI","JM SCI","JM SCI","HGE","MDN","JM SCI","JM SCI","JM SCI","JM SCI","JM SCI","MJ SCI","OFC","OFC","MJ SCI","OFC","OFC","MDN","VACIO","RZ","DAL","JM SCI","HGE","APT","MDN","NA ","APT","APT","SM","RZ","OFC","JM SCI","MJ SCI","OFC","HGE","OFC","OFC","SM","OFC","OFC","OFC","OFC","HGE","OFC","SM","OFC","OFC","OFC","NA","MDN","MJ SCI","HGE","MJ SCI","MJ SCI","OFC","OFC","DAL","DAL","DAL","DAL","SM","DAL","DAL","SM","DAL","DAL","MJ SCI","MJ SCI","MJ SCI","MDN","MDN","MDN","OFC","OFC","HGE","HGE","OFC","SM","SM","NA","APT","OFC","MDN","SM","OFC","JM SCI","MJ SCI","OFC","OFC","OFC","HGE","HGE","SM","OFC","MJ SCI","SM","MJ SCI","MJ SCI","OFC","MJ SCI","MJ SCI","DAL","OFC","OFC","OFC","JM SCI","OFC","OFC","RZ","OFC","OFC","SM","JM SCI","OFC","MJ SCI","SP SCI","OFC","OFC","OFC","MJ SCI","RZ","DAL","OFC","JM SCI","OFC","SP SCI","MJ SCI","MJ SCI","SP SCI","HGE","MJ SCI","MJ SCI","OFC","MJ SCI","RZ","MJ SCI","OFC","HGE","SP SCI","VACIO","MJ SCI","SM","OFC","SP SCI","OFC","OFC","OFC","VACIO","VACIO","OFC","SP SCI","DAL","RZ","OFC","OFC","DAL","RZ","OFC","MJ SCI","SP SCI","OFC","HGE","MJ SCI","OFC","OFC","OFC","DAL","VB SCI","SP SCI","JAP","OFC","OFC","JAP","JAP","JAP","JAP","MDN","VB SCI","VB SCI","OFC","OFC","OFC","DAL","OFC","DAL","DAL","JAP","OFC","OFC","ALAN  ","OFC","VB SCI","OFC","HGE","OFC","PIN","OFC","NA","OFC","MDN","HH SCI","HH SCI","VB SCI","VB SCI","OFC","OFC","RZ","CD SCI","OFC","OFC","OFC","HGE","HGE","OFC","NA","HGE","OFC","OFC","HGE","RZ","RZ","JAP","VACIO","OFC","HGE","HGE","VACIO","OFC","OFC","BEA","OFC","APT","OFC","OFC"]
+
+    df_clientes = pd.DataFrame({"Customer Name": clientes,
+
+
+                                "Salesperson ID": vendedores
+                                })
+
+    df_clientes['Salesperson ID'] = df_clientes['Salesperson ID'].str.replace(r'.*SCI.*', 'SCI', regex=True)
+
+    df = df[df["Compania"].isin(["FABRICA BRILUX C.A.'FABRICA BRILUX C.A.","FABRICA BRILUX C.A."])]
+
+    vendedores_evaluar = ["OFC", "APT", "HGE", "VACIO", "SCI", "MDN"]
+
+    salesperson_mapping = df_clientes.set_index('Customer Name')['Salesperson ID'].to_dict()
+
+    # Map the Salesperson ID only for matched customer names
+    df['Salesperson ID'] = df['Customer Name'].map(salesperson_mapping).fillna(df['Salesperson ID'])
+
+
+    df = df[df['Salesperson ID'].isin(vendedores_evaluar)]
+
+    names_to_remove = [
+        'ABRAHAM WAINBEG',
+        'HILDYBEL COROMOTO VALERA PERNALETE',
+        'SANTAMARIA PACHECO ODRA',
+        'EMILSE FERNANDEZ',
+        'JIMMY POCATERRA',
+        'SANDRA SIOMARA SANCHEZ CASTRO',
+        'ANAKARINA FERMIN R',
+        'RUTH NOEMI BLANCO RUIZ',
+        'JOHN HENRY PEREZ BARON',
+        'RIVILLO DIAZ,MARIA DE LOS ANGELES',
+        'SOFIA G SANTANIELLO JAIMES',
+        'SANCHEZ CASTRO SANDRA SIOMARA',
+        'GIUSMARI STELLA TARANTINO LUNA',
+        'LEYMAR ELIZABETH FERMIN GERARDO'
+    ]
+
+    # Remove rows where 'Customer Name' is in the list of names to remove
+    df = df[~df['Customer Name'].isin(names_to_remove)]
+
+    reference_date = datetime.now()
+    last_25_days_date = reference_date - timedelta(days=25)
+    last_190_days_date = reference_date - timedelta(days=250)
+
+    cutoff_date = datetime.today() - timedelta(days=250)
+
+    # Group by 'Customer Name' and filter out those with the earliest 'Document Date' older than 190 days
+    filtered_customers = df.groupby('Customer Name')['Document Date'].max()
+    filtered_customers = filtered_customers[filtered_customers >= cutoff_date].index
+
+    # Filter the original DataFrame to include only the relevant customers
+    filtered_df = df[df['Customer Name'].isin(filtered_customers)]
+
+    cutoff_date = datetime.today() - timedelta(days=25)
+
+    # Group by 'Customer Name' and find the maximum 'Document Date' for each
+    latest_customers = filtered_df.groupby('Customer Name')['Document Date'].max()
+
+    # Filter out customers with a 'Document Date' greater than 25 days from today
+    latest_customers = latest_customers[latest_customers <= cutoff_date].index
+
+    # Filter the original DataFrame to include only the relevant customers
+    filtered_df = filtered_df[filtered_df['Customer Name'].isin(latest_customers)]
+
+
+
+    customers_with_recent_purchases = filtered_df["Customer Name"].unique()
+
+    df_filtered = df[df['Customer Name'].isin(customers_with_recent_purchases)]
+
+
+    salesperson_customer_details = {}
+
+    if not df_filtered.empty:
+        for customer in df_filtered['Customer Name'].unique():
+            customer_records = df_filtered[df_filtered['Customer Name'] == customer].sort_values('Document Date')
+            last_salesperson_id = customer_records.iloc[-1]['Salesperson ID']
+
+            if last_salesperson_id not in salesperson_customer_details:
+                salesperson_customer_details[last_salesperson_id] = {}
+
+            if customer not in salesperson_customer_details[last_salesperson_id]:
+                salesperson_customer_details[last_salesperson_id][customer] = []
+
+            for product in customer_records['Item Description'].unique():
+                product_records = customer_records[customer_records['Item Description'] == product]
+                total_days = 0
+                total_qty_per_day = 0
+                count_intervals = 0
+
+                for i in range(len(product_records) - 1):
+                    current_purchase = product_records.iloc[i]
+                    next_purchase = product_records.iloc[i + 1]
+
+                    days_until_next_purchase = (next_purchase['Document Date'] - current_purchase['Document Date']).days
+                    if days_until_next_purchase > 0:
+                        qty_per_day = current_purchase['QTY'] / days_until_next_purchase
+                        total_days += days_until_next_purchase
+                        total_qty_per_day += qty_per_day
+                        count_intervals += 1
+
+                if count_intervals > 0:
+                    avg_qty_per_day = total_qty_per_day / count_intervals
+                else:
+                    avg_qty_per_day = 0
+
+                agotado = None
+
+                estimated_consumption = round((reference_date - product_records.iloc[-1]['Document Date']).days, 0) * round(avg_qty_per_day, 2)
+
+                if estimated_consumption >= round(product_records.iloc[-1]['QTY'], 0) *0.9:
+
+                  agotado = "Agotado"
+
+                else:
+
+                  agotado = "Posible Inventario"
+
+                product_detail = {
+                    'Product': product,
+                    'Days Since Last Purchase': round((reference_date - product_records.iloc[-1]['Document Date']).days, 0),
+                    'Quantity Last Purchased': round(product_records.iloc[-1]['QTY'], 0),
+                    'Highest Purchase Amount': round(product_records['QTY'].max(), 0),
+                    'Purchase Count': len(product_records),
+                    'Product Recurrence (Avg QTY/Day)': round(avg_qty_per_day, 2),
+                    'Estimated Consumption': estimated_consumption,
+                    "Agotado": agotado
+                }
+
+                salesperson_customer_details[last_salesperson_id][customer].append(product_detail)
+
+
+    def prepare_salesperson_data(df):
+        # Group by Salesperson ID and Customer Name, and find the most recent purchase date
+        recent_purchase = df.groupby(['Salesperson ID', 'Customer Name'])['Document Date'].max().reset_index()
+
+        # Calculate days since last purchase
+        recent_purchase['days_last_purchase'] = (datetime.now() - recent_purchase['Document Date']).dt.days
+
+        # Sort by days_last_purchase in descending order
+        recent_purchase.sort_values(by=['Salesperson ID', 'days_last_purchase'], ascending=[True, False], inplace=True)
+
+        # Create a dictionary of DataFrames for each salesperson
+        salesperson_dfs = {k: v for k, v in recent_purchase.groupby('Salesperson ID')}
+
+        return salesperson_dfs
+
+    salesperson_dfs = prepare_salesperson_data(df)
+
+
+
+
+    vs = st.selectbox("Vendedor", vendedores_evaluar)
+
+
+
+    with st.expander("Clientes"):
+        cs = st.multiselect("", salesperson_customer_details[vs].keys(), salesperson_customer_details[vs].keys())
+
+    for x in cs:
+        st.dataframe(pd.DataFrame(salesperson_customer_details[vs][x]))
 
 else:
   pass
